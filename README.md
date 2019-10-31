cbdee5e575a11b3ea4102779691613deb3b9bebb
HoangPham <pham.hoang1591@gmail.com>
2019-10-31 14:13:53 +0200

    add get_next_line.c, header get_next_line.h, library libft

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # get_next_line
 In this project I have to write function get_next_line which read a line with end of line ('\n') or end of file ('\0) from a file descriptor.
+For more information you can read from get_next_line.en.pdf file
---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hopham <hopham@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/25 10:08:13 by hopham            #+#    #+#             */
+/*   Updated: 2019/10/30 12:28:40 by hopham           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include "./libft/includes/libft.h"
+
+# define BUFF_SIZE 32
+# define MAX_FD 4864
+
+int	get_next_line(const int fd, char **line);
+
+#endif
